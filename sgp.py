0ad35ce4fb6f16cbcc06d7c069768fdeb01262df
Miłosz Meller <meller.milosz@gmail.com>
2017-08-21 18:49:01 +0200

    Added sgp.py

---
+++ sgp.py
@@ -0,0 +1,52 @@
+#! /usr/bin/python
+
+# Finds a random solution for g-4-w instances of the Social Golfer Problem given g and trying to maximise w
+
+from random import shuffle, choice
+from itertools import combinations
+from sys import argv
+
+TABLES = 10 if len(argv) == 1 else int(argv[1])
+NO_OF_PLAYERS = 4 * TABLES
+FAILURES_LIMIT = 1_000_000
+
+def random_tables(players):
+    # Returns a list of TABLES random tables from possible_tables such that no player is included in more than one table
+    # or None if fails to find a valid set within FAILURES_LIMIT tries
+    tables = []
+    options = possible_tables.copy()
+    failures = 0
+    while len(tables) < TABLES:
+        if len(options) == 0:
+            tables = []
+            options = possible_tables.copy()
+            failures += 1
+        if failures >= FAILURES_LIMIT:
+            return None
+        pick = choice(tuple(options))
+        tables.append(pick)
+        players = set(pick)
+        options = set(x for x in options if len(players & set(x)) == 0)
+    return tables
+
+def play_round(tables):
+    # Removes all tables from possible_tables that contain a pair included in any of the tables in tables
+    for table in tables:
+        pairs = set(combinations(table, 2))
+        global possible_tables
+        possible_tables = set(x for x in possible_tables if len(pairs & set(combinations(x,2))) == 0)
+
+if __name__ == '__main__':
+    players = list(range(NO_OF_PLAYERS))
+    possible_tables = set(combinations(players, 4))
+    rounds = 0
+    while True:
+        tables = random_tables(players)
+        if tables:
+            rounds += 1
+            print("Round %d\a" % rounds)
+            print(tables)
+            play_round(tables)
+        else:
+            print('---')
+            break
