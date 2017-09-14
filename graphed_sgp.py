2b7ee9e510e4e249859c19e9b90c394ce05327b2
Miłosz Meller <meller.milosz@gmail.com>
2017-09-14 13:29:51 +0200

    Cleaned up a bit and changed the output to be more readable.

--- graphed_sgp.py
+++ graphed_sgp.py
@@ -4,9 +4,8 @@
 # Might be slow and require a lot of memory for bigger problems
 
 import networkx as nx
-from networkx.algorithms.approximation.clique import max_clique
-from sys import argv
 from itertools import combinations, chain
+from sys import argv
 
 TABLES = 10 if len(argv) == 1 else int(argv[1])
 PLAYERS_AT_TABLE = 4 if len(argv) <= 2 else int(argv[2])
@@ -19,16 +18,12 @@ def all_pairs(tables):
 
 G = nx.complete_graph(NO_OF_PLAYERS)
 print("graph G generated")
-# print(G.nodes())
-# print(G.edges())
 H = nx.Graph()
 H.add_nodes_from(all_k_cliques(G, PLAYERS_AT_TABLE))
 for node, node2 in combinations(H.nodes(), 2):
     if len(set(node) & set(node2)) == 0:
         H.add_edge(node, node2)
 print("graph H generated")
-# print(H.nodes())
-# print(H.edges())
 I = nx.Graph()
 I.add_nodes_from(all_k_cliques(H, TABLES))
 print("graph I - added nodes")
@@ -41,16 +36,14 @@ for node, node2 in combinations(I.nodes(), 2):
             break
     if no_repeats:
         I.add_edge(node, node2)
-# print(I.nodes())
-# print(I.edges())
 print("graph I - added edges")
 k_max = int((NO_OF_PLAYERS - 1) / (PLAYERS_AT_TABLE - 1))
 found = False
 while not found:
     for solution in all_k_cliques(I, k_max):
         found = True
-        print(solution)
+        for round in solution:
+            print(round)
         print(k_max)
         break
     k_max -= 1
-# print(max_clique(I))
